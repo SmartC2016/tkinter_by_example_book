0b21ecf1b5d8ddc648db5e87425260ab15746c05
SmartC2016 <christian.hetmann@gmail.com>
2018-11-16 11:28:09 +0100

    Tkinter by example - ToDo App - Version 2

--- venv/todo_app_v2.py
+++ venv/todo_app_v2.py
@@ -1,5 +1,7 @@
 """
 This is the second version of the todo app from the book "Tkinter by Example by David Love, Jan 2018
+coded by: Christian Hetmann
+Last Edit: 16.11.2018
 """
 
 import tkinter as tk
